55dee87a65bcb11cfb979ebfa0536d7925176b7d
Sukhveer Kaur <sukhveerkaur9465@gmail.com>
2024-03-20 16:33:15 +0530

    changes new

---
+++ build/precache-manifest.218c595e0957b8eff7aa441ce62e178f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6434725d5f215b9269d05cd730e53cce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c6b03661e683d2de03c",
+    "url": "/static/css/2.c046a6b4.chunk.css"
+  },
+  {
+    "revision": "ad22875fc27a46a45ac9",
+    "url": "/static/css/main.0bd81b61.chunk.css"
+  },
+  {
+    "revision": "1c6b03661e683d2de03c",
+    "url": "/static/js/2.496158fe.chunk.js"
+  },
+  {
+    "revision": "87e2d83e199dad9b8b7f",
+    "url": "/static/js/3.158fcf4e.chunk.js"
+  },
+  {
+    "revision": "ad22875fc27a46a45ac9",
+    "url": "/static/js/main.f22895fa.chunk.js"
+  },
+  {
+    "revision": "d2d41835a685807d8747",
+    "url": "/static/js/runtime~main.3d84ad0d.js"
+  },
+  {
+    "revision": "d4943655e0256082dd91efb2f3ce0b58",
+    "url": "/static/media/bgs.d4943655.jpg"
+  },
+  {
+    "revision": "1d1e7c3ac149ca4ba2ab82246ac7d680",
+    "url": "/static/media/imgpsh_fullsize_anim.1d1e7c3a.png"
+  }
+]);
